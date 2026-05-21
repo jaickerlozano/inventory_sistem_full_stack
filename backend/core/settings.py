2db28b8f86f7f246ee434b131d79dc4d52e3e0cb
Jaicker Rafael Lozano Flores <jlozano.devcode@gmail.com>
2026-05-21 10:54:32 -0400

    feat(core): refactorizando urls.py

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -130,7 +130,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Configuración de CORS - Permitir todas las solicitudes CORS (no recomendado para producción)
 CORS_ALLOWED_ORIGINS = [
-    # "http://localhost:5173",
+    "http://localhost:5173",
     # "http://127.0.0.1:5173",
 ]
 
--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,26 +1,18 @@
-"""
-URL configuration for core project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
+from rest_framework import routers
+from inventory.views import ProductViewSet, CategoryViewSet, SupplierViewSet, StockMovementViewSet
+
+router = routers.DefaultRouter() # Create a router and register our viewsets with it.
+router.register(r'products', ProductViewSet, 'products')
+router.register(r'categories', CategoryViewSet, 'categories')
+router.register(r'suppliers', SupplierViewSet, 'suppliers')
+router.register(r'stock-movements', StockMovementViewSet, 'stock-movements')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/inventory/', include('inventory.urls')),
+    path('api/inventory/', include(router.urls)),
     # API schema (JSON o YAML)
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     # UI Swagger
