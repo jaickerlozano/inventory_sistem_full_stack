f0c816473f8c76a77fc403b9fa2763e2f8806492
Jaicker Rafael Lozano Flores <jlozano.devcode@gmail.com>
2026-05-25 21:52:26 -0400

    feat(core): incorporando django-filter

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'corsheaders',
     'drf_spectacular',
+    'django_filters',
 
     'inventory',
 ]
@@ -138,6 +139,8 @@ CORS_ALLOWED_ORIGINS = [
 REST_FRAMEWORK = {
     # ...
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination', # Paginación por número de página
+    'PAGE_SIZE': 10  # Solo 10 registros por petición
 }
 
 # Configuración de drf-spectacular
